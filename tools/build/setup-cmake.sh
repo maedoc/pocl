3f93c0275b59cc5e0d46174202c3bdae73b674bd
maedoc <marmaduke.woodman@univ-amu.fr>
2016-11-07 23:08:10 +0100

    always build cmake

--- tools/build/setup-cmake.sh
+++ tools/build/setup-cmake.sh
@@ -1,14 +1,8 @@
 #!/bin/bash
 
-# if we don't even have CMake, set that up
-if [ -z "$(which cmake)" ]
-then
-	echo "Setting up CMake in prefix '$prefix'."
-	mkdir -p $PREFIX/src && pushd $PREFIX/src
-	wget --no-check-certificate https://cmake.org/files/v3.6/cmake-3.6.2.tar.gz
-	tar xzf cmake*
-	cd cmake*
-	./bootstrap --prefix=$PREFIX && make -j && make install
-else
-	echo "CMake found at '$(which cmake)'."
-fi
+echo "Setting up CMake in prefix '$prefix'."
+mkdir -p $PREFIX/src && pushd $PREFIX/src
+wget --no-check-certificate https://cmake.org/files/v3.6/cmake-3.6.2.tar.gz
+tar xzf cmake*
+cd cmake*
+./bootstrap --prefix=$PREFIX && make -j && make install
